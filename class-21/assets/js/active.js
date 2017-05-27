374af5b21da5f38d2f23c5b9f1ba9556f869c81b
Nazmul Hasan <nazmuls4@yahoo.com>
2017-05-27 13:56:42 +0600

    class-21

---
+++ class-21/assets/js/active.js
@@ -0,0 +1,30 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+		 $('.homepage-slide').owlCarousel({
+			items: 1,
+			loop: true,
+			autoplay: true,
+			dots: true,
+			nav: false,				
+			
+		});
+        
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+       
+
+    });
+
+
+
+
+}(jQuery));
