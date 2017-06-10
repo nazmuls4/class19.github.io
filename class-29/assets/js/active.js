b5a0b82473957d3b7c5c1a6471b5c55f1e825d9b
Nazmul Hasan <nazmuls4@yahoo.com>
2017-06-10 23:46:35 +0600

    class-29

--- class-29/assets/js/active.js
+++ class-29/assets/js/active.js
@@ -5,14 +5,21 @@
     'use strict'; // use strict to start
 
     jQuery(document).ready(function($) {
-		
+		$(".list-filter li").on('click', function () {
+            $(".list-filter li").removeClass("active");
+            $(this).addClass("active");
+            var selector = $(this).attr('data-filter');
+            $(".project-item").isotope({
+                filter: selector
+            });
+        });
         
     });
 
 	
 
     jQuery(window).load(function() {
-       
+		$('.project-item').isotope();
 
     });
 
