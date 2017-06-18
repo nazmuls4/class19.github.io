0a25abda51c7942628acf48d6797b25067cc8af8
Nazmul Hasan <nazmuls4@yahoo.com>
2017-06-18 17:49:13 +0600

    class-31

---
+++ class-31/assets/js/active.js
@@ -0,0 +1,26 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+		var s = skrollr.init({
+		    render: function(data){
+		        
+		    }
+		});
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+		
+
+    });
+
+
+
+
+}(jQuery));
